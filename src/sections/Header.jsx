65dd971ab53471d19293d00014f94b4ad18fccef
JAmal Michael <eshimi090@gmail.com>
2024-06-10 15:29:51 -1200

    mobile-nav

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CiSearch } from 'react-icons/ci'
 import { Button } from "@/components/ui/button"
+import { FaBell } from "react-icons/fa"
 
 
 const Header = () => {
@@ -8,20 +9,34 @@ const Header = () => {
     <container className="flex justify-between items-center p-2" >
         <div className="">
         <h2 className="font-bold text-sm sm:text-xl">
-       <span >JAmal</span><span className="text-[#4d6dff] ">Michael</span>
+       <span >Alex</span><span className="text-blue-900 ">Williamson</span>
        </h2>
         </div>
-        <div>
+        <div className="hidden sm:block">
             searchbar
         </div>
         <div className="hidden sm:block">
         <Button>Click me</Button>
         </div> 
-        <div className="hidden sm:block">
-        alarm icon/notification
+        <div className='flex items-center relative w-[100%] mx-2'>
+        <div className='mr-0'>
+        <Button className="bg-white rounded-[100%] active:bg-white cursor-pointer">
+        <FaBell 
+        className='text-black'
+        />
+        </Button>
         </div>
-        <div>
-        profile
+        <div className='flex'>
+        <img 
+        src='../public/assets/profile-pic.jpg'
+        alt='profile-picture'
+        className='rounded-full object-cover w-10 h-10 cursor-pointer'
+        /> 
+        <div className='ml-1 text-left cursor-pointer'>
+        <p className='text-blue-900 font-semibold text-[12px]'>Alex Williamson</p>      
+        <p className=' text-[10px] font-light'>alexwilliamson@gmail.com</p>  
+        </div>     
+        </div>
         </div>
     </container>
   )
